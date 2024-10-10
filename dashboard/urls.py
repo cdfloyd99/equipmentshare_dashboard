95a292a126f3f685822dd58800597397dd43ef5f
cdfloyd99 <165749335+cdfloyd99@users.noreply.github.com>
2024-10-10 01:59:16 -0500

    correction to remove /reporting page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,6 +6,4 @@ from . import views
 urlpatterns = [
     path('', views.dashboard_view, name='dashboard'),
     path('pl-statement/', views.pl_statement_view, name='pl_statement'),
-    # Removed the reporting URL pattern
-    # path('reporting/', views.reporting_view, name='reporting'),
 ]
